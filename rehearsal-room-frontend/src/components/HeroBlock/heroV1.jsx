995932b12985acc6bd0ff9164474c2ca3c6d0b34
Adele Noronha <adele.c.noronha@gmail.com>
2021-07-29 00:26:48 +0000

    return hero component to pre-test state

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -1,15 +1,9 @@
 import React, {useEffect, useState} from "react";
-import { Component } from "react";
 import './_Hero.scss'
 
 
 export default function HeroV1(props) {
-  console.log('Component has started');
-
   const [currentImage, setCurrentImage] = useState("")
-
-  console.log(`Current image is ${currentImage}`);
-
   const bannerImgArr = [
     "https://res.cloudinary.com/dyo7gkw5s/image/upload/q_auto,w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1621230600/dance_mirror.jpg",
     "https://res.cloudinary.com/dyo7gkw5s/image/upload/q_auto,w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/dramatic_dude.jpg",
@@ -19,16 +13,13 @@ export default function HeroV1(props) {
   useEffect(() => {
     const selectImage = bannerImgArr[Math.floor(Math.random()* 3)]; //can genereate 0, 1 or 2
     setCurrentImage(selectImage);
-    console.log('useEffect changed image here')
-  }, [])
+  }, [bannerImgArr])
 
-  const output = (
+  return (
     <>
       <div id="banner-container">
       <img className="image" sizes="100vw" src={currentImage} alt="artist rehearsing banner"/>
       </div>
     </>
   )
-    console.log(`Coponent finished.`)
-  return output;
 }
