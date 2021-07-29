ad8205ce788b547c9a474196b828721cdb414873
Adele Noronha <adele.c.noronha@gmail.com>
2021-07-29 00:42:24 +0000

    remove bannerArr dependency

--- rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
+++ rehearsal-room-frontend/src/components/HeroBlock/heroV1.jsx
@@ -3,7 +3,7 @@ import './_Hero.scss'
 
 
 export default function HeroV1(props) {
-  const [currentImage, setCurrentImage] = useState("")
+  const [currentImage, setCurrentImage] = useState([])
   const bannerImgArr = [
     "https://res.cloudinary.com/dyo7gkw5s/image/upload/q_auto,w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1621230600/dance_mirror.jpg",
     "https://res.cloudinary.com/dyo7gkw5s/image/upload/q_auto,w_auto,c_fill,ar_3:1,g_center/l_text:Arial_120_bold:Artists Looking For Space. Spaces Available for Artists.,co_rgb:FFF/v1620667456/dramatic_dude.jpg",
@@ -13,7 +13,7 @@ export default function HeroV1(props) {
   useEffect(() => {
     const selectImage = bannerImgArr[Math.floor(Math.random()* 3)]; //can genereate 0, 1 or 2
     setCurrentImage(selectImage);
-  }, [bannerImgArr])
+  },[])
 
   return (
     <>
